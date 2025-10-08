45e9c13027c0180f45ff108eace5a5d26ef08bde
rakib8680 <rakib.cst.1st@gmail.com>
2025-10-08 18:50:50 +0600

    Added googleapis dependency and enhanced server.js with a new function to retrieve calendar events by date, including error handling and event data formatting.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import dotenv from "dotenv";
+import { google } from "googleapis";
 import z from "zod";
 import { version } from "zod/v4/core";
 
@@ -11,16 +12,51 @@ const server = new McpServer({
   version: "1.0.0",
 });
 
-
-//tool function 
+//tool function
 async function getMyCalendarDataByDate(date) {
-    const calendar = google.calendar({
-        version:'v3',
-        auth: process.env.GOOGLE_API_KEY
-    })
+  const calendar = google.calendar({
+    version: "v3",
+    auth: process.env.GOOGLE_API_KEY,
+  });
+
+  //calculate start and end of the day
+  const startOfDay = new Date(date);
+  startOfDay.setUTCHours(0, 0, 0, 0);
+  const endOfDay = new Date(date);
+  endOfDay.setUTCDate(endOfDay.getUTCDate() + 1);
+
+  try {
+    const res = await calendar.events.list({
+      calendarId: "primary",
+      timeMin: startOfDay.toISOString(),
+      timeMax: endOfDay.toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: "startTime",
+    });
+
+    const events = res.data.items || [];
+
+    const eventData = events.map((event) => {
+      const start = event.start.dateTime || event.start.date;
+      const end = event.end.dateTime || event.end.date;
+      return {
+        summary: event.summary,
+        start,
+        end,
+        description: event.description || "No description",
+        location: event.location || "No location",
+      };
+    });
+    if (eventData.length > 0) {
+      return eventData;
+    } else {
+      return "No events found for this date";
+    }
+  } catch (error) {
+    return { error: error.message || "Error fetching calendar data" };
+  }
 }
-
-
 
 //register the tool to mcp
 server.tool(
